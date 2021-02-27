aa8b05a490587adf316c51e812df0e2e2590f915
Bitwhys <bitwhys@gmail.com>
2021-02-27 01:48:12 -0800

    chore: creates test command to prototpye Comment resource

--- api/resources/comment.py
+++ api/resources/comment.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from flask_restful import Resource
+import googleapiclient.discovery
+
+GOOGLE_API_KEY = "AIzaSyB3i6xQSuki2gyAHnwAVanAsz4LyoaQ5vc"
+
+
+# Call the API's commentThreads.list method to list the existing comments.
+def get_comments_as_dict(youtube, video_id):
+    results = (
+        youtube.commentThreads()
+        .list(part="snippet", videoId=video_id, textFormat="plainText",)
+        .execute()
+    )
+    return results
 
 
 class Comment(Resource):
@@ -8,9 +21,25 @@ class Comment(Resource):
     TODO: describe the resource
     """
 
-    def get(self):
-        # TODO: implement GET /comment/<???>
-        pass
+    def get(self, video_id):
+        """
+        @rtype: object
+        """
+        api_service_name = "youtube"
+        api_version = "v3"
+        # client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+        # # Get credentials and create an API client
+        # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        #     client_secrets_file, scopes
+        # )
+        # credentials = flow.run_console()
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=GOOGLE_API_KEY
+        )
+
+        response = get_comments_as_dict(youtube, video_id)
+        return response
 
     def post(self):
         # TODO: implement POST /comment/<video_id>
---
+++ tests/test_comments.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secret.json"
+
+    # Get credentials and create an API client
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes
+    # )
+    # credentials = flow.run_console()
+    # credentials = {}
+    youtube = googleapiclient.discovery.build(
+        api_service_name,
+        api_version,
+        developerKey="AIzaSyB3i6xQSuki2gyAHnwAVanAsz4LyoaQ5vc",
+    )
+
+    # request = youtube.channels().list(
+    #     part="snippet,contentDetails,statistics",
+    #     id="UC_x5XG1OV2P6uZZ5FSM9Ttw",
+    #     developerKey="AIzaSyB3i6xQSuki2gyAHnwAVanAsz4LyoaQ5vc",
+    # )
+    request = youtube.commentThreads().list(
+        part="snippet", videoId="F-9Y4dJgAeE", textFormat="plainText",
+    )
+    response = request.execute()
+
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
